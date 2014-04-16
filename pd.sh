9a8dc44d55554cc2bd768c48d1e2e29f9c488e14
stewart <iwojima@163.com>
2014-04-16 11:35:01 +0800

    this is common tools

---
+++ pd.sh
@@ -0,0 +1,57 @@
+###########################################################################
+#
+# File:     $/XDK/DevKit/tools/pd.bat
+# Purpose:  quick jump to target dir & shorthand for pushd and popd
+# Usage:    pd [ -? | directory | ~ | @ | inc | lib | cls ]
+# Date:     2000-4-24
+#
+###########################################################################
+WORK_ROOT=/home/stewart/iWork
+
+if [ "$1" == "-h" ]; then
+    echo "usage: pd DIR     (same as PUSHD DIR)"
+    echo "       pd         (same as POPD)"
+    echo "       pd m       (PUSHD to mirror directory)"
+    echo "       pd p       (PUSHD to android package directory or msvc win32 app directory)"
+    echo "       pd @       (PUSHD to target directory)"
+    echo "       pd inc     (PUSHD to compiled INC directory)"
+    echo "       pd lib     (PUSHD to compiled LIB directory)"
+    echo "       pd cls     (PUSHD to compiled CLS directory)"
+elif [ "$1" == "" ]; then
+    popd
+elif [ "$1" == "@" ]; then
+    
+    pushd $WORK_ROOT/lib
+    #echo 'pwd = '`pwd`
+elif [ "$1" == "inc" ]; then
+    pushd $WORK_ROOT/include
+elif [ "$1" == "cls" ]; then
+    pushd $XDK_USER_OBJ/$XDK_BUILD_KIND/$1
+elif [ "$1" == "lib" ]; then
+    pushd $XDK_USER_OBJ/$XDK_BUILD_KIND/$1
+elif [ "$1" == "m" ]; then
+    PROJECT_DIR=$PWD
+#    temp_dir=${XDK_ROOT/XDK*/}
+    XDK_EMAKE_DIR=${PWD/$XDK_SOURCE_PATH/}
+    pushd $XDK_USER_OBJ/$XDK_BUILD_KIND/mirror$XDK_EMAKE_DIR
+elif [ "$1" == "p" ]; then
+    PROJECT_DIR=$PWD
+    temp_dir=`dirname $PROJECT_DIR`
+    XDK_EMAKE_DIR=${PROJECT_DIR/$temp_dir/}
+    if [ "$XDK_TARGET_PLATFORM" = "win32" ]; then
+        if [ -d $XDK_TARGETS$XDK_EMAKE_DIR ]; then
+            pushd $XDK_TARGETS$XDK_EMAKE_DIR
+        else
+            pushd $XDK_TARGETS
+        fi
+    elif [ "$XDK_TARGET_PLATFORM" = "openkode" ]; then
+        if [ -d $XDK_USER_OBJ/$XDK_BUILD_KIND/package$XDK_EMAKE_DIR ]; then
+            pushd $XDK_USER_OBJ/$XDK_BUILD_KIND/package$XDK_EMAKE_DIR
+        else
+            pushd $XDK_USER_OBJ/$XDK_BUILD_KIND/package
+        fi
+    fi
+else
+    pushd $1
+fi
+
---
+++ setenv.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+export IWORK_HOME=/home/stewart/iWork
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$IWORK_HOME/lib:$IWORK_HOME/Thrid_party/ACE_wrappers_linux/lib
+export PATH=$PATH:$IWORK_HOME/common/bin
+export ACE_ROOT=$IWORK_HOME/Thrid_party/ACE_wrappers
+
+alias cls=clear
+alias vi=vim
+alias s='gnome-open . > /dev/null'
+alias gedit='gedit $* > /dev/null'
+alias egrep='egrep --color'
+alias grep='grep --color'
+alias gb='cd /home/stewart'
+alias gd='cd /home/stewart/Desktop'
+alias pd='source /home/stewart/iWork/common/bin/pd.sh $*'
+alias work='source /home/stewart/iWork/common/bin/setenv.sh'
+alias emake='/home/stewart/iWork/common/bin/e_make.sh $*'
+alias e='emake'
+alias er='emake rebuild'
+
+export CURRENT_PATH=`pwd`
+export WORK_PATH=`echo $CURRENT_PATH | grep iWork`
+if [ -z $WORK_PATH ]; then
+	cd /home/`whoami`/iWork
+fi
+
+
+
